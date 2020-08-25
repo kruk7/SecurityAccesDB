ba05ad773138acb7d5c83b702ebcba6a5cdabf97
Raven <kamil.raven7@gmail.com>
2020-08-26 00:16:01 +0200

    add UserServlet impl.

--- src/main/java/ejb/UserRepository.java
+++ src/main/java/ejb/UserRepository.java
@@ -1,5 +1,18 @@
 package ejb;
 
+import entity.User;
+
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Stateless
 public class UserRepository
 {
+    @PersistenceContext
+    private EntityManager em;
+
+    public void add(User user)
+    { em.persist(user); }
 }
--- src/main/java/servlet/UserServlet.java
+++ src/main/java/servlet/UserServlet.java
@@ -1,5 +1,9 @@
 package servlet;
 
+import ejb.UserRepository;
+import entity.User;
+
+import javax.inject.Inject;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -7,16 +11,19 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "UserServlet")
+@WebServlet(name = "UserServlet", value = "/users")
 public class UserServlet extends HttpServlet
 {
+    @Inject
+    UserRepository userRepo;
+
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
-
-    }
-
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
-    {
-
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        String role = request.getParameter("role");
+        User user = new User(username, password, role);
+        userRepo.add(user);
+        response.sendRedirect(request.getContextPath());
     }
 }
